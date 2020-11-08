d0f4c13459b3775d60dea17fe4db700bbc1fa9a3
Byron <byrondavid87@gmail.com>
2020-11-08 15:10:05 -0800

    adding files.

---
+++ library.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        console.log(name + "by" + author + ", " + pages + " pages, not read yet.");
+    }
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function displayBooks() {
+    
+}
